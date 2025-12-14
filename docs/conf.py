363e6ed5634599650fe369ea8691ef471c1d1e28
Todd Proebsting <proebsting@gmail.com>
2025-12-13 22:05:16 -0700

    Bump version to 0.1.1 - add documentation

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = "PartiGraph"
 copyright = "2025, Todd Proebsting"
 author = "Todd Proebsting"
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
